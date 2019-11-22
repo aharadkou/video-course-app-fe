b42105729fcb5702fad138de5de785a5eca3c6ff
Aliaksandr Haradkou <Aliaksandr_Haradkou@epam.com>
2019-11-22 13:07:54 +0300

    Add some styles to the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CoursePageModule } from './course-page/course-page.module';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faPlayCircle as farPlayCircle} from '@fortawesome/free-regular-svg-icons';
+import { faPlayCircle as farPlayCircle, faClock as farClock, faCalendarAlt as farCalendarAlt, IconDefinition} from '@fortawesome/free-regular-svg-icons';
 import { faUser as fasUser, faSignOutAlt as fasSignOutAlt, faPlus as fasPlus, 
   faPen as fasPen, faTrash as fasTrash, faSearch as fasSearch} from '@fortawesome/free-solid-svg-icons';
+import { CoursePageContainerComponent } from './course-page/course-page-container/course-page-container.component';
+
+
+const routes : Routes = [
+  {
+    path: '',
+    component : CoursePageContainerComponent
+  }
+];
+
+const icons : IconDefinition[] = [
+  farPlayCircle, fasUser, fasSignOutAlt, fasPlus, 
+  fasPen, fasTrash, fasSearch, farClock, farCalendarAlt
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, CoreModule, CoursePageModule, FontAwesomeModule
+    BrowserModule, CoreModule, CoursePageModule, FontAwesomeModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor(library: FaIconLibrary) {
-    library.addIcons(farPlayCircle, fasUser, fasSignOutAlt, fasPlus, fasPen, fasTrash, fasSearch);
+    library.addIcons(...icons);
   }
 }
--- src/app/pipes/course-duration.pipe.ts
+++ src/app/pipes/course-duration.pipe.ts
@@ -5,10 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CourseDurationPipe implements PipeTransform {
 
-  transform(duration: number): string {
+  transform(value: number): string {
     const minutesInHour: number = 60;
-    let hoursNumber = Math.floor(duration / minutesInHour);
-    return (hoursNumber ? hoursNumber + 'h ': ' ') + (duration % minutesInHour) + ' min';
+    let hoursNumber = Math.floor(value / minutesInHour);
+    return (hoursNumber ? hoursNumber + 'h ': ' ') + (value % minutesInHour) + ' min';
   }
 
 }
