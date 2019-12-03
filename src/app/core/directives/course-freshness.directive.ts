097190a4aa79482d79763c23907ded10063f3ab1
Aliaksandr Haradkou <Aliaksandr_Haradkou@epam.com>
2019-12-03 09:20:34 +0300

    Fix date comparison

--- src/app/core/directives/course-freshness.directive.ts
+++ src/app/core/directives/course-freshness.directive.ts
@@ -9,12 +9,12 @@ export class CourseFreshnessDirective implements OnInit {
 
   @Input() set appCourseFreshness(creationDate: Date) {
     const currentDate = new Date();
-    const preCurrentDate = new Date();
+    const preCurrentDate = new Date(currentDate);
     preCurrentDate.setDate(currentDate.getDate() - 14);
-    if (creationDate.getTime() <= currentDate.getTime() && creationDate.getTime() >= preCurrentDate.getTime()) {
+    if (creationDate < currentDate && creationDate.getTime() >= preCurrentDate.getTime()) {
       this.renderer.addClass(this.elementRef.nativeElement, 'course-item-fresh');
     }
-    if (creationDate.getTime() > currentDate.getTime()) {
+    if (creationDate > currentDate) {
       this.renderer.addClass(this.elementRef.nativeElement, 'course-item-upcoming');
     }
   }
