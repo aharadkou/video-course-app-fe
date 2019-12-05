c6fefccb08e55d6e5cb97cd933552d451f437ef7
Aliaksandr Haradkou <Aliaksandr_Haradkou@epam.com>
2019-12-05 16:53:49 +0300

    Fix course freshness directive

--- src/app/core/directives/course-freshness.directive.ts
+++ src/app/core/directives/course-freshness.directive.ts
@@ -8,14 +8,14 @@ export class CourseFreshnessDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   @Input() set appCourseFreshness(creationDate: Date) {
+    const freshnessDuration = 14;
     const currentDate = new Date();
     const preCurrentDate = new Date(currentDate);
-    preCurrentDate.setDate(currentDate.getDate() - 14);
-    if (creationDate < currentDate && creationDate.getTime() >= preCurrentDate.getTime()) {
-      this.renderer.addClass(this.elementRef.nativeElement, 'course-item-fresh');
-    }
+    preCurrentDate.setDate(currentDate.getDate() - freshnessDuration);
     if (creationDate > currentDate) {
       this.renderer.addClass(this.elementRef.nativeElement, 'course-item-upcoming');
+    } else if (creationDate < currentDate && creationDate.getTime() >= preCurrentDate.getTime()) {
+      this.renderer.addClass(this.elementRef.nativeElement, 'course-item-fresh');
     }
   }
 
