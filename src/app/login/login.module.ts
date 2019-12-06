567bf29eb6cd8b3d5499f3167c91144469ff7669
Aliaksandr Haradkou <Aliaksandr_Haradkou@epam.com>
2019-12-06 09:56:06 +0300

    Add title to login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { CoreModule } from '../core/core.module';
 
 
 
 @NgModule({
   declarations: [LoginPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule
   ]
 })
 export class LoginModule { }
